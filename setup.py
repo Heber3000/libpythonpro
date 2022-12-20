005d98d6913de674302f000ae9d51b0709011ee3
heber3000 <heberlevy@gmail.com>
2022-12-20 17:52:59 -0300

    alteração do nome para publicação

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonpro-heber"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Heber Levy de Almeida"
