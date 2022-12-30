d7aa85f8c355dcf5418ce4c594b72774559bd4c6
heber3000 <heberlevy@gmail.com>
2022-12-30 16:48:19 -0300

    montando teste a montando pacotes

---
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,15 @@
+from libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario(sessao):
+    usuario= Usuario(nome='Heber')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+
+def test_listar_usuario(sessao):
+    usuarios = [Usuario(nome='Heber'), Usuario(nome='Levy')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
