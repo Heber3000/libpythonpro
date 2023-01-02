21cb8bac3606133d48735a19a3d394389efd9d08
heber3000 <heberlevy@gmail.com>
2023-01-02 17:18:37 -0300

    montando teste a montando pacotes

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,17 +2,10 @@ from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 import pytest
+from unittest.mock import Mock
 
 
 
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 @pytest.mark.parametrize(
     'usuarios',
     [
@@ -30,14 +23,14 @@ class EnviadorMock(Enviador):
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao,enviador)
     enviador_de_spam.enviar_emails(
         'heberlevy@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantasticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 
@@ -46,14 +39,14 @@ def test_qtd_de_spam(sessao, usuarios):
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Heber',email='heberlevy@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'hebergeologo@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantasticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'hebergeologo@gmail.com',
         'heberlevy@gmail.com',
         'Curso Python Pro',
